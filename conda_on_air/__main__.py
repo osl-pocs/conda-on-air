d3de2026cc4ed4f758f9677acb183720591834d4
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-05-22 12:01:11 -0400

    wip

--- conda_on_air/__main__.py
+++ conda_on_air/__main__.py
@@ -2,9 +2,9 @@
 Entrypoint module, in case you use `python -m conda-on-air`.
 Why does this file exist, and why __main__? For more info, read:
 - https://www.python.org/dev/peps/pep-0338/
-- https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
-"""from conda_on_air.cli import main  # type: ignore
+"""
+from conda_on_air.cli import main  # type: ignore
 
 if __name__ == '__main__':
     main()
--- conda_on_air/conda_on_air.py
+++ conda_on_air/conda_on_air.py
@@ -1,1 +1,6 @@
 """Main module."""
+
+class CondaOnAir:
+    config_data: dict = {}
+
+    def read_config(self):
